4180b2c7cae092a585501cbea7b8d17bdc51e6c2
Cotue <jovenegas@estudiantec.cr>
2024-09-05 17:10:05 -0600

    Ahora se implementara con matriz

--- TronPlay/Game1.cs
+++ TronPlay/Game1.cs
@@ -4,48 +4,45 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TronPlay
 {
-    public class Game1 : Game
-    {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Grid _grid;
-        public Game1()
+        public class Game1 : Game
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            private GraphicsDeviceManager _graphics;
+            private SpriteBatch _spriteBatch;
+
+            public Game1()
+            {
+                _graphics = new GraphicsDeviceManager(this);
+                Content.RootDirectory = "Content";
+                IsMouseVisible = true;
+            }
+
+            protected override void Initialize()
+            {
+                
+                base.Initialize();
+            }
+
+            protected override void LoadContent()
+            {
+                _spriteBatch = new SpriteBatch(GraphicsDevice);
+            }
+
+            protected override void Update(GameTime gameTime)
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+
+                base.Update(gameTime);
+            }
+
+            protected override void Draw(GameTime gameTime)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
+
+                _spriteBatch.Begin();
+                _spriteBatch.End();
+
+                base.Draw(gameTime);
+            }
         }
-
-        protected override void Initialize()
-        {
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _grid = new Grid(_spriteBatch);
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            _spriteBatch.Begin();
-            _grid.Draw();
-            _spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
-    }
 }
--- TronPlay/LinkedListNode.cs
+++ TronPlay/LinkedListNode.cs
@@ -1,1 +1,107 @@
-﻿
+﻿namespace TronPlay
+{
+    public class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+        public LinkedListNode(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private LinkedListNode<T> _head;
+        private LinkedListNode<T> _tail;
+
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        public void AddFirst(T value)
+        {
+            var newNode = new LinkedListNode<T>(value);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                newNode.Next = _head;
+                _head = newNode;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new LinkedListNode<T>(value);
+            if (_tail == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (_head == null)
+                return;
+
+            if (_head == _tail)
+            {
+                _head = null;
+                _tail = null;
+                return;
+            }
+
+            var current = _head;
+            while (current.Next != _tail)
+            {
+                current = current.Next;
+            }
+
+            current.Next = null;
+            _tail = current;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        public LinkedListNode<T> GetHead()
+        {
+            return _head;
+        }
+
+        public LinkedListNode<T> GetTail()
+        {
+            return _tail;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            var current = _head;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+    }
+}
+
